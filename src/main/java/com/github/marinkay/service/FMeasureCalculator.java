a560005e12eec72a582f171fa3155f315cfabb5c
Marina <marinkay666@gmail.com>
2024-12-02 17:52:07 +0300

    fallout and sout

--- src/main/java/com/github/marinkay/service/FMeasureCalculator.java
+++ src/main/java/com/github/marinkay/service/FMeasureCalculator.java
@@ -7,6 +7,7 @@ public class FMeasureCalculator {
     private int truePositive = 0;
     private int falsePositive = 0;
     private int falseNegative = 0;
+    private int trueNegative = 0;
 
     public void addResult(SpamClassifierEnum predicted, SpamClassifierEnum actual) {
         if (predicted == SpamClassifierEnum.SPAM && actual == SpamClassifierEnum.SPAM) {
@@ -15,13 +16,18 @@ public class FMeasureCalculator {
             falsePositive++;
         } else if (predicted == SpamClassifierEnum.NOT_SPAM && actual == SpamClassifierEnum.SPAM) {
             falseNegative++;
+        } else if (predicted == SpamClassifierEnum.NOT_SPAM && actual == SpamClassifierEnum.NOT_SPAM) {
+            trueNegative++;
         }
     }
 
     public double calculateFMeasure() {
         double precision = (truePositive + falsePositive) > 0 ? (double) truePositive / (truePositive + falsePositive) : 0;
+        System.out.println("Precision - " + precision);
         double recall = (truePositive + falseNegative) > 0 ? (double) truePositive / (truePositive + falseNegative) : 0;
-
+        System.out.println("Recall - " + recall);
+        double fallout = (falsePositive + trueNegative) > 0 ? (double) falsePositive / (falsePositive + trueNegative) : 0;
+        System.out.println("Fallout - " + fallout);
         return (precision + recall > 0) ? 2 * (precision * recall) / (precision + recall) : 0;
     }
 }
